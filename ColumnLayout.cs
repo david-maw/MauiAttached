d7068c3a2e75bee7d2817760efc33718aa5bd4d8
David Maw <david@maw.name>
2022-10-12 10:28:17 -0700

    Add project files.

---
+++ ColumnLayout.cs
@@ -0,0 +1,41 @@
+﻿//From https://github.com/hartez/CustomLayoutExamples#single-column-layout 
+using Microsoft.Maui.Layouts;
+
+namespace MauiAttached;
+
+public class ColumnLayout : VerticalStackLayout
+{
+    public static readonly BindableProperty FillProperty = BindableProperty.CreateAttached("Fill", typeof(bool),
+        typeof(ColumnLayout), false);
+
+    public ColumnLayout()
+    {
+    }
+
+    protected override ILayoutManager CreateLayoutManager()
+    {
+        return new ColumnLayoutManager(this);
+    }
+
+    // Support methods for the attached property
+    public bool GetFill(BindableObject bindableObject)
+    {
+        return (bool)bindableObject.GetValue(FillProperty);
+    }
+    
+    public void SetFill(BindableObject bindableObject, bool fill)
+    {
+        bindableObject.SetValue(FillProperty, fill);
+    }
+
+    // Convenience method for use from the layout manager
+    public bool GetFillForView(IView view)
+    {
+        if (view is BindableObject bindableObject)
+        {
+            return GetFill(bindableObject);
+        }
+
+        return false;
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiAttached;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+        columnLayout.SetFill(line2, true);
+    }
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+	{
+		columnLayout.SetFill(line2,!columnLayout.GetFill(line2));
+    }
+}
+
